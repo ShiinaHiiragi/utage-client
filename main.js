dfe7960362afe6c87368e94e913bfb33bc7e5ee7
Shiina <shiinahiiragi@outlook.com>
2021-05-09 14:05:13 +0800

    stage change

--- main.js
+++ main.js
@@ -1,36 +1,51 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const path = require('path');
+const electron = require('electron');
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+const app = electron.app;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+Menu.setApplicationMenu(null);
 
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    show: false,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true
     }
-  })
+  });
 
   if (!app.isPackaged)
   {
     // Open the DevTools and load the index.html of the app.
     mainWindow.loadURL('http://localhost:3000/');
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
   }
   else mainWindow.loadURL(path.join(__dirname, './build/index.html'));
+
+  // this must be called after the loadURL()
+  mainWindow.maximize();
+  mainWindow.show();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
   
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    if (BrowserWindow.getAllWindows().length === 0)
+      createWindow();
   })
 })
 
@@ -38,7 +53,7 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit();
 })
 
 // In this file you can include the rest of your app's specific main process
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log(window.require('electron').remote.process.version);
 ReactDOM.render(
   <React.StrictMode>
     <App />
