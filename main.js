9f2fafb7062e8eb2f62e4ebfe98c2df2cbe9c091
Shiina <shiinahiiragi@outlook.com>
2021-06-06 22:22:56 +0800

    subtle change

--- main.js
+++ main.js
@@ -128,7 +128,6 @@ app.on("window-all-closed", function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 ipcMain.on("uid", (event, proxy, uid) => {
-  console.log(uid, uid !== undefined);
   if (uid !== undefined)
     request({
       url: `${proxy}sign/out?userid=${uid}`,
